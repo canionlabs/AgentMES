11abacaadd2681636735d6f6277ae0d7275ef4fd
Ramon Melo <me@ramonmelo.com>
2018-08-28 00:14:44 -0300

    Add configuration file

---
+++ src/config.h
@@ -0,0 +1,33 @@
+// ███╗   ███╗███████╗███████╗
+// ████╗ ████║██╔════╝██╔════╝
+// ██╔████╔██║█████╗  ███████╗
+// ██║╚██╔╝██║██╔══╝  ╚════██║
+// ██║ ╚═╝ ██║███████╗███████║
+// ╚═╝     ╚═╝╚══════╝╚══════╝
+// Definitions
+//
+
+// Device
+
+#define INPUT_PIN D1
+#define TYPE_1 1
+
+// Cloud
+
+#define MQTT_BROKER "broker.shiftr.io"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "/mes"
+#define MQTT_USER "71b18f70"
+#define MQTT_PASS "5daf5773ea30f228"
+
+// MES
+
+#define MES_SERVER "http://mesadmin.canionlabs.io/api/receiver/"
+#define MES_ORGANIZATION "canionlabs001"
+#define MES_DEVICE_TYPE "linkit"
+#define MES_DEVICE_ID "teste-teste"
+
+// Connection
+
+#define WIFI_SSID "Automata"
+#define WIFI_PASS "data.hal.johnny"
